4fea0407b207df6cd085c9ae9d97223f965a1b14
zcyang-cs <48715541+zcyang-cs@users.noreply.github.com>
2024-04-22 15:59:01 +0800

    Update loo.py

--- Code/defense/loo.py
+++ Code/defense/loo.py
@@ -1,6 +1,3 @@
-
-# ++++++++++++++++++++++++++++++++++++defense++++++++++++++++++++++++++++++++++++
-# ++++++++++++++++++直接移除++++++++++++++++++
 def test_wo_malicious(self, malicious):
     # local model, global model移入GPU
     self.global_model.to(self.device)
@@ -12,15 +9,11 @@ def test_wo_malicious(self, malicious):
     correct = 0
     for j in tqdm(target_nodes):
         target_node = j.item()
-        # client产生embedding, 连接起来构成global embedding
         global_emb = self.communicate_wo_malicious(target_node, malicious)
 
-        # server接收global embedding, 返回预测
         pred = self.global_model(global_emb)
 
-        # 计算准确率acc
         outputS = F.log_softmax(pred, dim=-1)
-        # outputS[[0]]==outputS: (1, 7)         labels: (2708)
         correct += accuracy(outputS[[0]], self.labels[[target_node]]).item()
     acc = int(correct) / int(len(self.test_idx))
 
@@ -28,7 +21,7 @@ def test_wo_malicious(self, malicious):
 
 
 def communicate_wo_malicious(self, target_node, malicious):
-    aggregation = self.aggregation  # sum/avg
+    aggregation = self.aggregation  
     clients = [client for idx, client in enumerate(self.clients) if idx != malicious]
     for idx, client in enumerate(clients):
         client.local_model.eval()
