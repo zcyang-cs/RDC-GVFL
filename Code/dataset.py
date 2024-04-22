544152239c9cf391bc6f329840340904044b7549
zcyang-cs <48715541+zcyang-cs@users.noreply.github.com>
2024-04-22 15:55:07 +0800

    Update dataset.py

--- Code/dataset.py
+++ Code/dataset.py
@@ -10,45 +10,8 @@ import scipy.sparse as sp
 
 
 def load_data(para_dict):
-    """
-        # data_dict(torch_geometric)数据格式
-        'dataset': dataset,                         # Dataset(torch_geometric)
-        'data': data,                               # Data(torch_geometric)
-        'features': features,                       # tensor
-        'labels': labels,                           # tensor
-        'edge_index': edge_index,                   # tensor
-        'adj': adj,                                 # scipy.sparse._csr.csr_matrix
-        'train_idx': train_idx,                     # tensor
-        'val_idx': val_idx,                         # tensor
-        'test_idx': test_idx                        # tensor
-
-        # data_dict(deeprobust)数据格式
-        'dataset': dataset,                         # Dataset(DeepRobust)
-        'data': dataset,                            # Dataset(DeepRobust)
-        'features': features,                       # numpy.ndarray                 -> torch.sparse_tensor
-        'adj': adj,                                 # scipy.sparse._csr.csr_matrix  -> torch.sparse_tensor
-        'labels': labels,                           # numpy.ndarray                 -> torch.tensor
-        'idx_train': train_idx,                     # numpy.ndarray                 -> torch.tensor
-        'idx_val': val_idx,                         # numpy.ndarray                 -> torch.tensor
-        'idx_test': test_idx                        # numpy.ndarray                 -> torch.tensor
-    """
-
     if para_dict['dataset'] in ['Cora', 'Citeseer', 'Pubmed']:
         dataset_name = para_dict['dataset']
-        # method1: torch_geometric加载数据
-        # dataset = Planetoid(root='../data/raw_data', name='Cora')
-        # data = dataset[0]
-        #
-        # features = data.x                                   # features: (2708, 1433)
-        # labels = data.y                                     # labels: (2708)
-        # edge_index = data.edge_index                        # data.edge_index (2, 10556)
-        # adj = to_scipy_sparse_matrix(edge_index).tocsr()    # adj: (2708, 2708) coo -> csr
-        #
-        # train_idx = torch.argwhere(data.train_mask).squeeze()
-        # val_idx = torch.argwhere(data.val_mask).squeeze()
-        # test_idx = torch.argwhere(data.test_mask).squeeze()
-
-        # method2: deeprobust加载数据
         dataset = Planetoid(root='../data/raw_data', name=dataset_name)
         Dpr_dataset = Pyg2Dpr(dataset)
         Dpr_data = Dpr_dataset
@@ -59,18 +22,6 @@ def load_data(para_dict):
         train_idx, val_idx, test_idx = Dpr_data.idx_train, Dpr_data.idx_val, Dpr_data.idx_test
 
         data_dict = {
-            # data_dict(torch_geometric)数据格式
-            # 'dataset': dataset,                     # Dataset(torch_geometric)
-            # 'data': data,                           # Data(torch_geometric)
-            # 'features': features,                   # tensor
-            # 'labels': labels,                       # tensor
-            # 'edge_index': edge_index,               # tensor
-            # 'adj': adj,                             # scipy.sparse._csr.csr_matrix
-            # 'train_idx': train_idx,                 # tensor
-            # 'val_idx': val_idx,                     # tensor
-            # 'test_idx': test_idx                    # tensor
-
-            # data_dict(deeprobust)数据格式, 非tensor通过utils.to_tensor转成tensor(稀疏变量->稀疏张量,普通变量->普通张量)
             'dataset': Dpr_dataset,                     # Dataset(deeprobust)
             'data': Dpr_data,                           # Dataset(deeprobust)
             'features': features,                       # numpy.ndarray
@@ -84,7 +35,6 @@ def load_data(para_dict):
         return data_dict
 
     if para_dict['dataset'] == 'Cora_ML':
-        # csr存储的 ['indices', 'indptr', 'format', 'shape', 'data']
         adj_loader = np.load("../data/raw_data/Cora_ML/raw/cora_ml_adj.npz")
         adj = sp.csr_matrix((adj_loader['data'], adj_loader['indices'],
                              adj_loader['indptr']), shape=adj_loader['shape'])
@@ -103,7 +53,6 @@ def load_data(para_dict):
         Dpr_data = 'Cora_ML'
 
         data_dict = {
-            # data_dict(deeprobust)数据格式, 非tensor通过utils.to_tensor转成tensor(稀疏变量->稀疏张量,普通变量->普通张量)
             'dataset': Dpr_dataset,         # Dataset(deeprobust)
             'data': Dpr_data,               # Dataset(deeprobust)
             'features': features,           # numpy.ndarray
@@ -118,7 +67,6 @@ def load_data(para_dict):
 
 
 def partition(para_dict, data_dict):
-    # todo: 定义其他的划分方式,不同的划分方式也会对应不同的log;建立不同数据集和log的对应关系
     client_num = para_dict['num_clients']
     features = data_dict['features']
 
@@ -129,5 +77,3 @@ def partition(para_dict, data_dict):
     data_dict['features_list'] = features_list
 
     return data_dict
-
-
